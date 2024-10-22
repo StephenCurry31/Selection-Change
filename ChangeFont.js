0680fb689624e44635e26b8d337f0d6162a7a557
Daniel Pang <118140678+danielpang2020@users.noreply.github.com>
2024-10-22 10:24:01 +0800

    Update ChangeFont.js

--- ChangeFont.js
+++ ChangeFont.js
@@ -1,1 +1,11 @@
-
+function importFont(font){
+  const Poppins = document.createElement('style');
+  style.innerHTML = `
+  @import url('https://danielpang2020.github.io/FilesForCodes/PoppinsFont.css');
+  `;
+  if (font === 'Poppins') {
+    document.head.appendChild(Poppins);
+  } else {
+    alert('Font not found');
+  }
+}
